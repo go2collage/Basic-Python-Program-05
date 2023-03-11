f8124d21276965f6f28aff8b841c79d67d68f2e3
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:33:34 +0530

    Add files via upload

---
+++ BP_P5.py
@@ -0,0 +1,13 @@
+# Basic Python Program
+
+# Area and Perimeter of Square
+
+side = float(input("Enter side of Square: "))
+
+Area = side * side      # formual of Area of Square
+
+Perimeter = 4 * side    # formula of Perimeter of Square
+
+print("Area of Square is: ", Area)
+print("Perimeter of Square is: ", Perimeter)
+
